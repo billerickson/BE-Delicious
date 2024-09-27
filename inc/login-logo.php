71de1917171dcc5eec1f46071459ef1753d05122
Bill Erickson <bill.erickson@gmail.com>
2024-09-27 16:29:52 -0500

    login logo

--- inc/login-logo.php
+++ inc/login-logo.php
@@ -25,15 +25,12 @@ add_filter( 'login_headertext', '__return_empty_string' );
 function be_login_logo() {
 
 	$logo_path   = '/assets/icons/logo/logo.svg';
-	$logo_width  = 212;
-	$logo_height = 40;
 
 	if ( ! file_exists( get_theme_file_path( $logo_path ) ) ) {
 		return;
 	}
 
 	$logo   = get_theme_file_uri( $logo_path );
-	$height = floor( $logo_height / $logo_width * 312 );
 	$styles = sprintf(
 		'.login h1 a {
 			background-image: url(%s);
@@ -43,12 +40,11 @@ function be_login_logo() {
 			display: block;
 			overflow: hidden;
 			text-indent: -9999em;
-			width: 312px;
-			height: %dpx;
+			width: 120px;
+			height: 120px;
 		}',
 		esc_url( $logo ),
-		$height
 	);
-	wp_add_inline_style( 'theme-style', $styles );
+	echo '<style>' . $styles . '</style>';
 }
-//add_action( 'login_head', 'be_login_logo' );
+add_action( 'login_head', 'be_login_logo' );
